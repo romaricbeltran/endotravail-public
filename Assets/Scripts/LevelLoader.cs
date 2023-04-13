10607e64da3cb699c5f65f29bfbbf0032a780bfd
Romaric Beltran <romaric.beltran@gmail.com>
2023-04-13 21:53:53 +0200

    Fix end, fix links, fix tuto pos, play on background

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -25,46 +25,40 @@ public class LevelLoader : MonoBehaviour
     // Home Screen Transition
     public Animator homeScreenTransition;
 
-    public void ResetGame() {
-        StartCoroutine(LoadNonAdressableAsynchronously(0));
-    }
-
-    public void FirstLevel()
-    {
-        homeScreenTransition.SetTrigger("Start");
-        StartCoroutine(LoadFirstLevel());
-    }
-
-    IEnumerator LoadFirstLevel()
-    {
-        // Start CrossFade animation
-        yield return new WaitForSeconds(transitionTime*1.5f);
-        LoadLevel(0);
-    }
-
     public void LoadLevel(int addressIndex)
     {
         // Changement du texte en fonction de la scène chargée
         switch (addressIndex)
         {
             case 0:
-                levelTitleText.text = "CHAPITRE 1";
-                levelSubtitleText.text = "L'absence";
+                levelTitleText.text = "";
+                levelSubtitleText.text = "";
+                StartCoroutine(LoadNonAddressableAsynchronously(0));
                 break;
             case 1:
-                levelTitleText.text = "CHAPITRE 2";
-                levelSubtitleText.text = "Une journée type dans la peau d'une personne atteinte d'endométriose";
+                levelTitleText.text = "CHAPITRE 1";
+                levelSubtitleText.text = "L'absence";
+                homeScreenTransition.SetTrigger("Start");
+                StartCoroutine(LoadAddressableAsynchronously(addressIndex-1));
                 break;
             case 2:
-                levelTitleText.text = "CHAPITRE 3";
-                levelSubtitleText.text = "L'annonce au manager";
+                levelTitleText.text = "CHAPITRE 2";
+                levelSubtitleText.text = "Une journée type dans la peau d'une personne atteinte d'endométriose";
+                StartCoroutine(LoadAddressableAsynchronously(addressIndex-1));
                 break;
             case 3:
-                levelTitleText.text = "CHAPITRE 4";
-                levelSubtitleText.text = "Le rendez-vous avec la médecine du travail";
+                levelTitleText.text = "CHAPITRE 3";
+                levelSubtitleText.text = "L'annonce au manager";
+                StartCoroutine(LoadAddressableAsynchronously(addressIndex-1));
                 break;
             case 4:
+                levelTitleText.text = "CHAPITRE 4";
+                levelSubtitleText.text = "Le rendez-vous avec la médecine du travail";
+                StartCoroutine(LoadAddressableAsynchronously(addressIndex-1));
+                break;
+            case 5:
                 levelTitleText.text = "";
+                StartCoroutine(LoadNonAddressableAsynchronously(1));
                 break;
             default:
                 levelTitleText.text = null;
@@ -72,10 +66,9 @@ public class LevelLoader : MonoBehaviour
         }
 
         GameManager.GAME_PROGRESSION = levelTitleText.text;
-        StartCoroutine(LoadAddressableAsynchronously(addressIndex));
     }
 
-    IEnumerator LoadNonAdressableAsynchronously(int sceneIndex)
+    IEnumerator LoadNonAddressableAsynchronously(int sceneIndex)
     {
         // Start CrossFade animation
         loadingScreenTransition.SetTrigger("Start");
@@ -94,29 +87,17 @@ public class LevelLoader : MonoBehaviour
         }
     }
 
-    IEnumerator LoadAddressableLabelAsynchronously(string labelName)
-    {
-        AsyncOperationHandle<IList<GameObject>> loadingAssetOperation = Addressables.LoadAssetsAsync<GameObject>(labelName, null);
-        Debug.Log("Loading Assets Label " + labelName);
-
-        while (!loadingAssetOperation.IsDone)
-        {
-            float progress = Mathf.Clamp01(loadingAssetOperation.PercentComplete);
-            //Debug.Log(loadingAssetOperation.PercentComplete);
-            loadingBar.value = progress;
-
-            yield return null;
-        }
-
-        yield return loadingAssetOperation;
-    }
-
     IEnumerator LoadAddressableAsynchronously(int addressIndex)
     {
+        // Wait for Credits (homeAnimation)
+        if (addressIndex == 1)
+        {
+            yield return new WaitForSeconds(transitionTime * 1.5f);
+        }
+
         // Start CrossFade animation
         loadingScreenTransition.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);
-
         retardedUI.SetActive(true);
         
         switch (addressIndex)
@@ -154,6 +135,23 @@ public class LevelLoader : MonoBehaviour
 
         SceneInstance sceneInstance = loadingSceneOperation.Result;
         sceneInstance.ActivateAsync();
+    }
+
+    IEnumerator LoadAddressableLabelAsynchronously(string labelName)
+    {
+        AsyncOperationHandle<IList<GameObject>> loadingAssetOperation = Addressables.LoadAssetsAsync<GameObject>(labelName, null);
+        Debug.Log("Loading Assets Label " + labelName);
+
+        while (!loadingAssetOperation.IsDone)
+        {
+            float progress = Mathf.Clamp01(loadingAssetOperation.PercentComplete);
+            //Debug.Log(loadingAssetOperation.PercentComplete);
+            loadingBar.value = progress;
+
+            yield return null;
+        }
+
+        yield return loadingAssetOperation;
     }
 
     public void OpenLink(string openURL)
